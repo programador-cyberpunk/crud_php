d08908bd1274a3c99e565d5ad757081d0719cc50
programador-cyberpunk <hugosolidsnake@gmail.com>
2025-09-03 19:16:09 -0300

    ta menos bugado

--- src/pages/user/read.php
+++ src/pages/user/read.php
@@ -1,22 +1,41 @@
-<?php 
-require_once '../../config.php';
-require_once '../../src/actions.user.php';
-require_once '../../src/modules/messages.php';
-require_once '../../src/modules/header.php';
+    <?php 
+    require_once '../../config.php';
+    require_once '../../src/actions.user.php';
+    require_once '../../src/modules/messages.php';
+    require_once '../../src/modules/header.php';
 
-$users = readUserAction($conn);
-?>
+    $users = readUserAction($conn);
+    ?>
 
-<div class="container">
- <div class="row">
-    <a href="../../../"><h1>Users - Read</h1></a>
-    <a class="btn btn-sucess text-white" href="./creat.php">New</a>
- </div>
- <div class="row flex-center">
- <?php if(isset($_GET['message'])) echo(printMessage($_GET['message'])); ?>
- </div>
-        <table class="table-users">
-            
-        </table>
+    <div class="container">
+    <div class="row">
+        <a href="../../../"><h1>Users - Read</h1></a>
+        <a class="btn btn-sucess text-white" href="./creat.php">New</a>
+    </div>
+    <div class="row flex-center">
+    <?php if(isset($_GET['message'])) echo(printMessage($_GET['message'])); ?>
+    </div>
+            <table class="table-users">
+            <tr></tr> 
+                <th>NOME</th>
+                <th>EMAIL</th>
+                <th>TELEFONE</th>
+            </tr>
+    <?php foreach ($users as $row): ?>
+        <tr>
+        <td class="user-name"><?=htmlspeecialchars($row['name'])?></td>
+        <td class="user-name"><?=htmlspeecialchars($row['email'])?></td>
+        <td class="user-name"><?=htmlspeecialchars($row['fone'])?></td>
 
-</div>
+        <a classes="btn btn-primary text-white" href="./edit.php?id=<?=$row['id']?>">Editar</a>
+        </td>
+
+        <td>
+            <a class="btn btn-danger text-white" href="./delete.php?id=<?=$row['id']?>">Remover</a>
+        </td>
+        </tr>
+    <?php endforeach; ?>       
+            </table>
+    </div>
+
+    <?php require_once '../../src/partials/footer.php';?>
